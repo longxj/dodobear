251c45c11592553e0b8b5aef259c3592f0208f18
longxj <longxjgm@gmail.com>
2013-06-04 09:43:18 +0800

    Update shutil.sh

--- shutil.sh
+++ shutil.sh
@@ -1,6 +1,8 @@
-cmddirname="./longcmdot"
+cmddirname=""
 function InitLongCmd()
 {
+  cmddirname=$(cat /proc/sys/kernel/random/uuid)
+  
   if [ -d $cmddirname ];then
     rm -rf $cmddirname
   fi
@@ -10,15 +12,18 @@ function InitLongCmd()
 
 function ExecCmd()
 {
-  local filename=$1
+  local filename=$cmddirname"/"$1
+  local flagfilename=$filename".flag"
   local cmd=$2
-  $cmd > $cmddirname"/"$filename &
+  $cmd > $filename && touch $flagfilename&
 }
 
 function FetchOutput()
 {
   local filepath=$cmddirname"/"$1
-  until [ -s $filepath"" ]
+  local flagfilepath=$filepath".flag"
+    
+  until [ -f $flagfilepath ]
   do 
     sleep 1
   done
